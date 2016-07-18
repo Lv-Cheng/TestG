186982fe662e192d1d5deeeb6a15ec1cbbe8cfb9
hzx <simple0907@vip.qq.com>
2016-07-18 13:49:50 +0800

    add mtextview3

--- MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
@@ -4,23 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+    private TextView mTextView1;
     private TextView mTextView2;
+    private TextView mTextView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text_view_1);
-        mTextView.setText(R.string.app_name);
+        mTextView1 = (TextView) findViewById(R.id.text_view_1);
+        mTextView1.setText(R.string.app_name);
 
         mTextView2 = (TextView) findViewById(R.id.text_view_2);
         mTextView2.setText(R.string.app_name);
+
+        mTextView3 = (TextView) findViewById(R.id.text_view_3);
+        mTextView3.setText(R.string.app_name);
 
 
     }
